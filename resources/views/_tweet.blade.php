73d29d7d052d6d688b866e16aad1ab34ddecedcc
Hazel <hazelbracken@gmail.com>
2020-11-23 22:49:50 +0000

    design the timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,18 @@
+<div class="flex p-4 border border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+
+    </div>
+
+    <div>
+        <h5 class="font-bol mb-4">Frances Black</h5>
+
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, in dico novum quaeque eam, legendos electram te usu. Nonumes intellegam te vix, mel posse nostrum scaevola ea. Ei discere habemus noluisse ius, te mel meis partem inermis, mei in delectus sententiae. Liber explicari te duo, id liber urbanitas nec. In his munere vituperatoribus, id wisi euripidis vel.
+        </p>
+    </div>
+</div>
